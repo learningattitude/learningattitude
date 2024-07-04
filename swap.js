e56220cfcc83736aa7ff5a3643d8c1f491d598e3
Learning Attitude <162717406+learningattitude@users.noreply.github.com>
2024-07-04 05:53:04 +0530

    update
    add properly comment

--- swap.js
+++ swap.js
@@ -1,7 +1,7 @@
 let data_a = "This is value of A";
 let data_b = "This is value of B";
 
-// using destructure
+// Using Array Destructuring
 [data_a,data_b] = [data_b,data_a];
 
 console.log(`A = "${data_a}" and B = ${data_b}`)
