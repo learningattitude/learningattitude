c3757e5866eb60a1fb6cff76bfcd99710fa9a5b1
Learning Attitude <162717406+learningattitude@users.noreply.github.com>
2024-07-05 11:53:23 +0530

    Update plandrome.js
    create new paildrome function with example

--- plandrome.js
+++ plandrome.js
@@ -1,3 +1,15 @@
-const plandrome = (parm)=>{
-    
-}
+//nitin > nitin
+//131 > 131
+const getPliandrome = (param)=>{
+    let getType = typeof param;
+    let result;
+    if( getType === 'number'){
+      if(param < 0){return "Please enter valid number."} 
+      result = param.toString().split("").reverse().join("") === param.toString() ? `${param} is pliandrome number` : `${param} is not pliandrome number` ;
+    }
+    if(typeof param === 'string'){
+      result = param.toLowerCase().split("").reverse().join("") === param.toLowerCase() ? `${param} is pliandrome` : `${param} is not pliandrome` ;
+    }
+    return result
+  }
+  console.log(getPliandrome("nitin"))
